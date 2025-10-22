602260e555d0b31dd5d8ffbee325181dc6b580d5
Damien Menanteau <damien.menanteau@gmail.com>
2025-10-22 18:06:01 +0200

    Modernize C++ namespace declarations

--- cpp/include/kata/roman_numerals/RomanNumerals.hpp
+++ cpp/include/kata/roman_numerals/RomanNumerals.hpp
@@ -24,15 +24,9 @@ SOFTWARE.
 #include <kata/roman_numerals/config.hpp>
 #include <string>
 
-namespace com {
-namespace murex {
-namespace kata {
-namespace roman_numerals {
+namespace com::murex::kata::roman_numerals {
 
 ROMAN_NUMERALS_API std::string add(const std::string number1, const std::string number2);
 
-} // namespace roman_numerals
-} // namespace kata
-} // namespace murex
-} // namespace com
+} // namespace com::murex::kata::roman_numerals
 
--- cpp/src/kata/roman_numerals/RomanNumerals.cpp
+++ cpp/src/kata/roman_numerals/RomanNumerals.cpp
@@ -24,18 +24,12 @@ SOFTWARE.
 #include <kata/roman_numerals/RomanNumerals.hpp>
 #include <string>
 
-namespace com {
-namespace murex {
-namespace kata {
-namespace roman_numerals {
+namespace com::murex::kata::roman_numerals {
 
 std::string add(const std::string number1, const std::string number2)
 {
    return "";
 }
 
-} // namespace roman_numerals
-} // namespace kata
-} // namespace murex
-} // namespace com
+} // namespace com::murex::kata::roman_numerals
 
